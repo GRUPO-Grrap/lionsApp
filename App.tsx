ef1bbed1c71019d42c6f6f30e93bcb53c930d0f0
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-09-19 22:38:42 -0400

    fix(components)remove inputSelect component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import ButtonForm from "./src/components/button";
 
 export default function App() {
   return (
