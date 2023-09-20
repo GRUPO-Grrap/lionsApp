36c7c3aac1d15be74ee7f65a61a476cfafbb7dbf
Gabriel Lucas <69332675+gabrielgt5@users.noreply.github.com>
2023-09-19 22:49:36 -0400

    feat(login)create login screen

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ButtonForm from "./src/components/button";
+import LoginScreen from "./src/screens/login";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
